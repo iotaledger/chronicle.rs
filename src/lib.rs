27b8f0274768521698369d4a94cd7082322d2d0b
Yu-Wei Wu <wusyong9104@gmail.com>
2019-09-04 21:50:54 +0800

    Init commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use crossbeam::channel::unbounded;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_channel() {
+        // Make a structure to create Sender and Receiver
+        let c = Chronicle::new();
+        
+        // Send a tx to it, let Sender send to Receiver
+        let tx = [0; 2673];
+        c.recv(tx.clone());
+
+        // show that Receiver actually get the tx
+        assert_eq!(c.queue(), tx);
+    }
+}
