57fe960172a8a69429485b509a0915fbcc74de0b
bingyanglin <bingyang.lin@iota.org>
2019-09-05 18:19:59 +0800

    Format all

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ mod tests {
     fn test_create_channel() {
         // Make a structure to create Sender and Receiver
         let c = Chronicle::new();
-        
+
         // Send a tx to it, let Sender send to Receiver
         let tx = [0; 2673];
         c.recv(tx.clone());
