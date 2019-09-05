b9ddd371a01fa4b1e5f7c5c403b14414434d4167
Wu Yu Wei <wusyong9104@gmail.com>
2019-09-05 16:54:58 +0800

    Add unbounded channel (#3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,28 @@
-use crossbeam::channel::unbounded;
+// TODO: This is temporary lint, remove it once lib.rs has implementation
+#![allow(unused_imports)]
+
+pub mod subscriber;
+pub mod validator;
+
+use subscriber::Subscriber;
+use validator::Validator;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_create_channel() {
-        // Make a structure to create Sender and Receiver
-        let c = Chronicle::new();
-        
-        // Send a tx to it, let Sender send to Receiver
-        let tx = [0; 2673];
-        c.recv(tx.clone());
+    fn test_mpmc() {
+        let (s, r) = crossbeam_channel::unbounded();
+        let sub = Subscriber::new(s);
+        let val = Validator::new(r);
+        let sub2 = sub.clone();
+        let val2 = val.clone();
 
-        // show that Receiver actually get the tx
-        assert_eq!(c.queue(), tx);
+        sub.sender.send("Hello World!").unwrap();
+        sub2.sender.send("Chronicle start Scribing!").unwrap();
+
+        assert_eq!(val2.receiver.recv(), Ok("Hello World!"));
+        assert_eq!(val.receiver.recv(), Ok("Chronicle start Scribing!"));
     }
 }
