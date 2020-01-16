fb3fc3a552d912705ba8a9babbfb9bc5f23415f3
Yu-Wei Wu <wusyong9104@gmail.com>
2020-01-16 15:24:32 +0800

    Update storage interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,150 @@
-// TODO: This is temporary lint, remove it once lib.rs has implementation
-#![allow(unused_imports)]
+use std::{collections::HashMap, collections::HashSet, rc::Rc};
 
-use futures::channel::mpsc;
-
-pub mod bundle;
-pub mod subscriber;
-pub mod transaction;
-
-use bundle::Bvalidator;
-use subscriber::Subscriber;
-use transaction::Tvalidator;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use futures::executor::block_on;
-    use futures::sink::SinkExt;
-    use futures::stream::StreamExt;
-    use std::thread;
-
-    #[test]
-    fn test_mpmc() {
-        let (t, r) = crossbeam_channel::unbounded();
-        let (t2, _) = mpsc::channel(1);
-        let sub = Subscriber::new(t);
-        let val = Tvalidator::new(r, t2);
-        let sub2 = sub.clone();
-        let val2 = val.clone();
-
-        sub.tx.send("Hello World!").unwrap();
-        sub2.tx.send("Chronicle start Scribing!").unwrap();
-
-        assert_eq!(val2.rx.recv(), Ok("Hello World!"));
-        assert_eq!(val.rx.recv(), Ok("Chronicle start Scribing!"));
-    }
-
-    #[test]
-    fn test_mpsc_multi_thread() {
-        const AMT: u32 = 10000;
-        const NTHREADS: u32 = 8;
-        let (_, r) = crossbeam_channel::unbounded();
-        let (t2, r2) = mpsc::channel(0);
-        let txn = Tvalidator::new(r, t2);
-        let bundle = Bvalidator::new(r2);
-
-        let t = thread::spawn(move || {
-            let result: Vec<_> = block_on(bundle.rx.collect());
-            assert_eq!(result.len(), (AMT * NTHREADS) as usize);
-            for item in result {
-                assert_eq!(item, 1);
-            }
-        });
-
-        for _ in 0..NTHREADS {
-            let mut txn = txn.clone();
-
-            thread::spawn(move || {
-                for _ in 0..AMT {
-                    block_on(txn.tx.send(1)).unwrap();
-                }
-            });
-        }
-
-        drop(txn);
-        t.join().ok().unwrap();
+/// A transaction hash. To be replaced later with whatever implementation is required.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct TxHash(u64);
+impl TxHash {
+    pub fn is_genesis(&self) -> bool {
+        todo!()
     }
 }
+
+/// A transaction address. To be replaced later with whatever implementation is required.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct TxAddress(u64);
+
+/// [https://github.com/iotaledger/bee-rfcs/pull/20]
+pub struct Tx {
+    hash: TxHash,
+    trunk: TxHash,
+    branch: TxHash,
+    body: (),
+}
+
+/// A milestone hash. To be replaced later with whatever implementation is required.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct MilestoneHash(u64);
+impl MilestoneHash {
+    //Returns true if the milestone is the genesis of the Tangle
+    pub fn is_genesis(&self) -> bool {
+        todo!()
+    }
+}
+
+pub struct Milestone {
+    hash: MilestoneHash,
+    index: u32,
+}
+
+pub enum StorageError {
+    TransactionAlreadyExist,
+    TransactionNotFound,
+    UnknownError,
+    //...
+}
+
+pub enum ConnectionError {
+    InvalidUrl,
+    UnknownError,
+    //...
+}
+
+type HashesToApprovers = HashMap<TxHash, Vec<TxHash>>;
+type MissingHashesToRCApprovers = HashMap<TxHash, Vec<Rc<TxHash>>>;
+//This is a mapping between an iota address and it's balance change
+//practically, a map for total balance change over an addresses will be collected
+//per milestone (snapshot_index), when we no longer have milestones, we will have to find
+//another way to decide on a check point where to store an address's delta if we want to snapshot
+type StateDeltaMap = HashMap<TxAddress, i64>;
+
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Connection<Conn> {
+    async fn establish_connection(url: &str) -> Result<Conn, ConnectionError>;
+    async fn destroy_connection(connection: Conn) -> Result<(), ConnectionError>;
+}
+
+
+#[async_trait]
+pub trait StorageBackend {
+    //**Operations over transaction's schema**//
+    async fn insert_transaction(&self, tx: &Tx) -> Result<(), StorageError>;
+    async fn find_transaction(&self, tx_hash: TxHash) -> Result<Tx, StorageError>;
+    async fn update_transactions_set_solid(
+        &self,
+        transaction_hashes: HashSet<TxHash>,
+    ) -> Result<(), StorageError>;
+    async fn update_transactions_set_snapshot_index(
+        &self,
+        transaction_hashes: HashSet<TxHash>,
+        snapshot_index: u32,
+    ) -> Result<(), StorageError>;
+    async fn delete_transactions(&self, transaction_hashes: HashSet<TxHash>) -> Result<(), StorageError>;
+
+    //This method is heavy weighted and will be used to populate Tangle struct on initialization
+    fn map_existing_transaction_hashes_to_approvers(
+        &self,
+    ) -> Result<HashesToApprovers, StorageError>;
+    //This method is heavy weighted and will be used to populate Tangle struct on initialization
+    fn map_missing_transaction_hashes_to_approvers(
+        &self,
+    ) -> Result<MissingHashesToRCApprovers, StorageError>;
+
+    //**Operations over milestone's schema**//
+    async fn insert_milestone(&self, milestone: &Milestone) -> Result<(), StorageError>;
+    async fn find_milestone(&self, milestone_hash: MilestoneHash) -> Result<Milestone, StorageError>;
+    async fn delete_milestones(
+        &self,
+        milestone_hashes: HashSet<MilestoneHash>,
+    ) -> Result<(), StorageError>;
+
+    //**Operations over state_delta's schema**//
+    async fn insert_state_delta(
+        &self,
+        state_delta: StateDeltaMap,
+        index: u32,
+    ) -> Result<(), StorageError>;
+
+    async fn load_state_delta(&self, index: u32) -> Result<StateDeltaMap, StorageError>;
+}
+
+pub struct Storage<Conn: Connection<Conn>> {
+    pub connection:   Conn,
+}
+
+/*
+impl Storage<DummyConnection> {
+    async fn establish_connection(&mut self, url: &str) -> Result<(), ConnectionError> {
+        self.connection = DummyConnection::establish_connection(url)?;
+        Ok(())
+    }
+    async fn destroy_connection(connection: DummyConnection) -> Result<(), ConnectionError> {
+        DummyConnection::destroy_connection(connection);
+        Ok(())
+    }
+}
+
+pub struct DummyConnection {}
+
+impl DummyConnection {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Connection<DummyConnection> for DummyConnection {
+    async fn establish_connection(url: &str) -> Result<DummyConnection, ConnectionError> {
+        Ok(DummyConnection::new())
+    }
+    async fn destroy_connection(connection: DummyConnection) -> Result<(), ConnectionError> {
+        Ok(())
+    }
+}
+
+type DummyStorage = Storage<DummyConnection>;
+*/
+
+//impl StorageBackend for DummyStorage {
+    //Implement all methods here
+//}
