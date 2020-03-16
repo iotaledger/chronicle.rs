427827ae34eb3fe4344681c1d196915aaed6c0d3
bingyanglin <bingyang.lin@iota.org>
2020-03-16 17:39:45 +0800

    Add cql connection function body

--- src/connection/cql.rs
+++ src/connection/cql.rs
@@ -1,6 +1,17 @@
-use crate::ring::ring::{Msb, ShardCount};
-use tokio::net::TcpStream;
 use crate::cluster::supervisor::Tokens;
+use crate::ring::ring::{Msb, NodeId, ShardCount, Token, DC};
+use byteorder::{BigEndian, ByteOrder};
+use cdrs::compression::Compression;
+use cdrs::frame::traits::FromCursor;
+use cdrs::frame::Frame;
+use cdrs::frame::Opcode;
+use cdrs::frame::{frame_supported, IntoBytes};
+use std::collections::HashMap;
+use std::io::Cursor;
+use tokio::io::Error;
+use tokio::io::ErrorKind;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 
 pub type Address = String;
 
@@ -23,18 +34,57 @@ impl CqlConn {
         self.stream.take().unwrap()
     }
 }
-#[derive(Debug)]
-pub struct Error; // todo: change it to tokio error
 
 pub async fn connect(address: &Address) -> Result<CqlConn, Error> {
     // connect using tokio and return
-
+    let mut stream = TcpStream::connect(address.clone()).await?;
     // establish cql using startup frame and ensure is ready
-
-    // send options frame and decode supported frame as options
+    let mut shard: u8 = 0;
+    let mut nr_shard: u8 = 0;
+    let mut ignore_msb: u8 = 0;
+    {
+        let stream_copy = &mut stream;
+        let shard_copy = &mut shard;
+        let nr_shard_copy = &mut nr_shard;
+        let ignore_msb_copy = &mut ignore_msb;
+        let (mut socket_rx, mut socket_tx) = tokio::io::split(stream_copy);
+        let ref mut compression = Compression::None;
+        let startup_frame = Frame::new_req_startup(compression.as_str()).into_cbytes();
+        socket_tx.write(startup_frame.as_slice()).await?;
+        let mut ready_buffer = vec![0; 9];
+        socket_rx.read(&mut ready_buffer).await?;
+        if Opcode::from(ready_buffer[4]) != Opcode::Ready {
+            return Err(Error::new(ErrorKind::Other, "CQL connection failed."));
+        }
+        // send options frame and decode supported frame as options
+        let option_frame = Frame::new_req_options().into_cbytes();
+        socket_tx.write(option_frame.as_slice()).await?;
+        let mut head_buffer = vec![0; 9];
+        socket_rx.read(&mut head_buffer).await?;
+        let length = BigEndian::read_u32(&head_buffer[5..9]) as usize;
+        let mut body_buffer = vec![0; length];
+        socket_rx.read(&mut body_buffer).await?;
+        let mut cursor: Cursor<&[u8]> = Cursor::new(&body_buffer);
+        let options = frame_supported::BodyResSupported::from_cursor(&mut cursor)
+            .unwrap()
+            .data;
+        *shard_copy = options.get("SCYLLA_SHARD").unwrap()[0].parse().unwrap();
+        *nr_shard_copy = options.get("SCYLLA_NR_SHARDS").unwrap()[0].parse().unwrap();
+        *ignore_msb_copy = options.get("SCYLLA_SHARDING_IGNORE_MSB").unwrap()[0]
+            .parse()
+            .unwrap();
+    }
 
     // create cqlconn
-    todo!()
+    // TDO: Get the tokens
+    let cqlconn = CqlConn {
+        stream: Some(stream),
+        tokens: None,
+        shard_id: shard,
+        shard_count: nr_shard,
+        msb: ignore_msb,
+    };
+    Ok(cqlconn)
 }
 pub async fn fetch_tokens(connection: Result<CqlConn, Error>) -> Result<CqlConn, Error> {
     // fetch tokens from scylla using select query to system.local table,
@@ -49,13 +99,13 @@ pub async fn connect_to_shard_id(address: &Address, shard_id: u8) -> Result<CqlC
             Ok(cqlconn) => {
                 if cqlconn.shard_id == shard_id {
                     // return
-                    break Ok(cqlconn)
+                    break Ok(cqlconn);
                 } else if shard_id >= cqlconn.shard_count {
                     // error as it's impossible to connect to shard_id doesn't exist
-                    break Err(Error)
+                    break Err(Error::new(ErrorKind::Other, "shard_id does not exist."));
                 } else {
                     // continue to retry
-                    continue
+                    continue;
                 }
             }
             err => {
@@ -63,5 +113,4 @@ pub async fn connect_to_shard_id(address: &Address, shard_id: u8) -> Result<CqlC
             }
         }
     }
-
 }
