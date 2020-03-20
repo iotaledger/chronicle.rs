6b2a7b561725f8b3c81df6d56a63f54d5a929a5e
bingyanglin <bingyang.lin@iota.org>
2020-03-20 19:09:16 +0800

    Fix the data type in RowTokens for cdrs macro usage

--- src/connection/cql.rs
+++ src/connection/cql.rs
@@ -2,18 +2,17 @@ use crate::cluster::supervisor::Tokens;
 use crate::ring::ring::{Msb, NodeId, ShardCount, Token, DC};
 use cdrs::compression::Compression;
 use cdrs::frame::traits::FromCursor;
-
 use cdrs::frame::IntoBytes;
-use cdrs::types::from_cdrs::FromCDRSByName;
-use cdrs::types::prelude::{List, Value, Row, Bytes};
-use cdrs::types::AsRustType;
-
-use cdrs::frame::{Frame, Flag, TryFromRow};
 use cdrs::frame::Opcode;
-use cdrs::frame::{frame_supported,frame_result};
+use cdrs::frame::{frame_result, frame_supported};
+use cdrs::frame::{Flag, Frame, TryFromRow};
 use cdrs::query;
+use cdrs::types::from_cdrs::FromCDRSByName;
+use cdrs::types::prelude::{Bytes, List, Row, Value};
+use cdrs::types::AsRustType;
 use std::collections::HashMap;
 use std::io::Cursor;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use tokio::io::Error;
 use tokio::io::ErrorKind;
 use tokio::net::TcpStream;
@@ -46,8 +45,8 @@ impl CqlConn {
 
 #[derive(Clone, Debug, IntoCDRSValue, TryFromRow, PartialEq)]
 struct RowTokens {
- rpc_address: String,
- tokens: Vec<String>,
+    rpc_address: IpAddr,
+    tokens: Vec<String>,
 }
 
 pub async fn connect(address: &Address) -> Result<CqlConn, Error> {
@@ -93,27 +92,50 @@ pub async fn fetch_tokens(mut connection: Result<CqlConn, Error>) -> Result<CqlC
     // fetch tokens from scylla using select query to system.local table,
     // then add it to cqlconn
     // query param builder
-    let params = query::QueryParamsBuilder::new()
-    .page_size(500)
-    .finalize();
+    let params = query::QueryParamsBuilder::new().page_size(500).finalize();
     let query = "SELECT rpc_address, tokens FROM system.local;".to_string();
-    let query = query::Query{query, params};
+    let query = query::Query { query, params };
     // query_frame
     let query_frame = Frame::new_query(query, vec![Flag::Ignore]).into_cbytes();
     // write frame to stream
-    connection.as_mut().unwrap().stream.as_mut().unwrap().write(query_frame.as_slice()).await?;
+    connection
+        .as_mut()
+        .unwrap()
+        .stream
+        .as_mut()
+        .unwrap()
+        .write(query_frame.as_slice())
+        .await?;
     // read buffer
     let mut head_buffer = vec![0; 9];
-    connection.as_mut().unwrap().stream.as_mut().unwrap().read(&mut head_buffer).await?;
+    connection
+        .as_mut()
+        .unwrap()
+        .stream
+        .as_mut()
+        .unwrap()
+        .read(&mut head_buffer)
+        .await?;
     let length = get_body_length_usize(&head_buffer);
     let mut body_buffer = vec![0; length];
-    connection.as_mut().unwrap().stream.as_mut().unwrap().read(&mut body_buffer).await?;
+    connection
+        .as_mut()
+        .unwrap()
+        .stream
+        .as_mut()
+        .unwrap()
+        .read(&mut body_buffer)
+        .await?;
     let mut cursor: Cursor<&[u8]> = Cursor::new(&body_buffer);
     let rows = frame_result::ResResultBody::from_cursor(&mut cursor)
-        .unwrap().into_rows().unwrap();
+        .unwrap()
+        .into_rows()
+        .unwrap();
     for row in &rows {
-        let r = RowTokens::try_from_row(row.clone());
-        println!("{:?}",r);
+        let r = RowTokens::try_from_row(row.clone()).unwrap();
+        let rpc_address = r.rpc_address.to_string();
+        let tokens = r.tokens;
+        println!("{:?}, {:?}", rpc_address, tokens);
     }
     todo!()
 }
@@ -142,8 +164,8 @@ pub async fn connect_to_shard_id(address: &Address, shard_id: u8) -> Result<CqlC
 }
 
 fn get_body_length_usize(buffer: &[u8]) -> usize {
-    ((buffer[5] as usize) << 24) +
-    ((buffer[6] as usize) << 16) +
-    ((buffer[7] as usize) <<  8) +
-    ((buffer[8] as usize) <<  0)
+    ((buffer[5] as usize) << 24)
+        + ((buffer[6] as usize) << 16)
+        + ((buffer[7] as usize) << 8)
+        + ((buffer[8] as usize) << 0)
 }
