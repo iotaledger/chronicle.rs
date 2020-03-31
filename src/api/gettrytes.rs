aeaec1f2761d03e07afbdb73b89a82d28521a125
bingyanglin <bingyang.lin@iota.org>
2020-03-31 11:35:15 +0800

    Modify comments

--- src/api/gettrytes.rs
+++ src/api/gettrytes.rs
@@ -33,6 +33,7 @@ impl GetTrytesBuilder {
 }
 
 // Borrow the function from the storage crate
+// TODO: remove the comment and refactory
 fn decode_bytes(u8_slice: &[u8], num_trits: usize) -> TritBuf {
     let decoded_column_i8_slice: &[i8] = cast_slice(u8_slice);
     unsafe {
