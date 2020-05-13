2d76bec7dd2265c02db7b3dcd2e35b460e38f560
bingyanglin <bingyang.lin@iota.org>
2020-05-13 15:32:16 +0800

    Add result const

--- chronicle-cql/src/frame/result.rs
+++ chronicle-cql/src/frame/result.rs
@@ -1,2 +1,5 @@
-pub const VOID: i32 = 0x0001;
-pub const ROWS: i32 = 0x0002;
+pub const Void: i32 = 0x0001;
+pub const Rows: i32 = 0x0002;
+pub const SetKeyspace: i32 = 0x0003;
+pub const Prepared: i32 = 0x0004;
+pub const SchemaChange: i32 = 0x0005;
