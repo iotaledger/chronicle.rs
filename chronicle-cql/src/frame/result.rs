861da67f06de6eba4bed24505e2b5c4ae30bd079
bingyanglin <bingyang.lin@iota.org>
2020-05-13 15:37:48 +0800

    Typo

--- chronicle-cql/src/frame/result.rs
+++ chronicle-cql/src/frame/result.rs
@@ -1,5 +1,5 @@
-pub const Void: i32 = 0x0001;
-pub const Rows: i32 = 0x0002;
-pub const SetKeyspace: i32 = 0x0003;
-pub const Prepared: i32 = 0x0004;
-pub const SchemaChange: i32 = 0x0005;
+pub const VOID: i32 = 0x0001;
+pub const ROWS: i32 = 0x0002;
+pub const SETKEYSPACE: i32 = 0x0003;
+pub const PREPARED: i32 = 0x0004;
+pub const SCHEMACHANGE: i32 = 0x0005;
