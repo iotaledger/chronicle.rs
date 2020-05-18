884e75d9a90aec98f632ff128cad1e7b15072291
bingyanglin <bingyang.lin@iota.org>
2020-05-19 01:21:18 +0800

    Polish bundles.rs

--- chronicle-api/src/api/findtransactions/bundles.rs
+++ chronicle-api/src/api/findtransactions/bundles.rs
@@ -5,26 +5,11 @@ use chronicle_cql::{
     },
     rows,
 };
+use std::collections::HashSet;
 rows!(
-    rows: Hashes {},
+    rows: Hashes {hashes: HashSet<String>},
     row: Row(
-        Hash,
-        Payload,
-        Address,
-        Value,
-        ObsoleteTag,
-        Timestamp,
-        CurrentIndex,
-        LastIndex,
-        Bundle,
-        Trunk,
-        Branch,
-        Tag,
-        AttachmentTimestamp,
-        AttachmentTimestampLower,
-        AttachmentTimestampUpper,
-        Nonce,
-        Milestone
+        Hash
     ),
     column_decoder: BundlesDecoder
 );
@@ -34,7 +19,7 @@ trait Rows {
     // to decode the rows
     fn decode(self) -> Self;
     // to finalize it as the expected result (trytes or none)
-    fn finalize(self) -> Option<String>;
+    fn finalize(self) -> Option<HashSet<String>>;
 }
 
 impl Rows for Hashes {
@@ -44,11 +29,12 @@ impl Rows for Hashes {
         // return
         self
     }
-    fn finalize(self) -> Option<String> {
+    fn finalize(mut self) -> Option<HashSet<String>> {
         // check if result was not empty
         if self.rows_count == 1 {
-            // the buffer is ready to be converted to string trytes
-            Some(String::from_utf8(self.decoder.into_buffer()).unwrap())
+            self.hashes
+                .insert(String::from_utf8(self.decoder.into_buffer()).unwrap());
+            Some(self.hashes)
         } else {
             // we didn't have any transaction row for the provided hash.
             None
@@ -59,85 +45,5 @@ impl Rows for Hashes {
 impl BundlesDecoder for Hash {
     fn decode_column(start: usize, length: i32, acc: &mut Hashes) {
         acc.buffer().copy_within(start..(start + length as usize), 81)
-    }
-}
-impl BundlesDecoder for Payload {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the payload to get the hashes, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Address {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the address to get the hashes, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Value {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the value to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for ObsoleteTag {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the value to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Timestamp {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the timestamp to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for CurrentIndex {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the currentindex to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for LastIndex {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the lastindex to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Bundle {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the bundle to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Trunk {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the trunk to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Branch {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the branch to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Tag {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the tag to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for AttachmentTimestamp {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the attachmenttimestamp to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for AttachmentTimestampLower {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the attachmenttimestamplower to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for AttachmentTimestampUpper {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the attachmenttimestampupper to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Nonce {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the nonce to get the hash, so nothing should be done.
-    }
-}
-impl BundlesDecoder for Milestone {
-    fn decode_column(_start: usize, _length: i32, _acc: &mut Hashes) {
-        // we don't need the milestone to get the hash, so nothing should be done.
     }
 }
