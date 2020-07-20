935924ab49b9df21a55a0d1af67f8e51e45050b9
Louay Kamel <lk@overdb.com>
2020-07-20 16:02:21 -0400

    make hint as enum

--- chronicle-api/src/api/findtransactions/hints.rs
+++ chronicle-api/src/api/findtransactions/hints.rs
@@ -28,31 +28,69 @@ use serde::{
 };
 
 #[derive(Deserialize, Serialize, Clone)]
-pub struct Hint {
-    address: Option<Trytes81>,
-    tag: Option<Trytes27>,
-    paging_state: Option<Vec<u8>>,
-    year: u16,
-    month: u8,
+#[serde(untagged)]
+pub enum Hint {
+    Address {
+        address: Trytes81,
+        year: u16,
+        month: u8,
+        paging_state: Option<Vec<u8>>,
+    },
+    Tag {
+        tag: Trytes27,
+        year: u16,
+        month: u8,
+        paging_state: Option<Vec<u8>>,
+    },
 }
 
 impl Hint {
     pub fn new_address_hint(address: Trytes81, paging_state: Option<Vec<u8>>, year: u16, month: u8) -> Self {
-        Self {
-            address: Some(address),
-            tag: None,
+        Self::Address {
+            address,
             paging_state,
             year,
             month,
         }
     }
     pub fn get_vertex(&self) -> &[u8] {
-        if self.address.is_some() {
-            &self.address.as_ref().unwrap().0
-        } else if self.tag.is_some() {
-            &self.tag.as_ref().unwrap().0
-        } else {
-            unreachable!("get_vertex in hint struct")
+        match self {
+            Self::Address { address, .. } => &address.0,
+            Self::Tag { tag, .. } => &tag.0,
+        }
+    }
+    pub fn take_paging_state(&mut self) -> Option<Vec<u8>> {
+        match self {
+            Self::Address { paging_state, .. } => paging_state.take(),
+            Self::Tag { paging_state, .. } => paging_state.take(),
+        }
+    }
+    pub fn replace_paging_state(&mut self, pgs: Vec<u8>) {
+        match self {
+            Self::Address { paging_state, .. } => {
+                paging_state.replace(pgs);
+            }
+            Self::Tag { paging_state, .. } => {
+                paging_state.replace(pgs);
+            }
+        }
+    }
+    pub fn is_paging_state_some(&self) -> bool {
+        match self {
+            Self::Address { paging_state, .. } => paging_state.is_some(),
+            Self::Tag { paging_state, .. } => paging_state.is_some(),
+        }
+    }
+    pub fn year(&self) -> u16 {
+        match self {
+            Self::Address { year, .. } => *year,
+            Self::Tag { year, .. } => *year,
+        }
+    }
+    pub fn month(&self) -> u8 {
+        match self {
+            Self::Address { month, .. } => *month,
+            Self::Tag { month, .. } => *month,
         }
     }
 }
@@ -84,7 +122,7 @@ impl Rows for Hashes {
     fn finalize(mut self) -> (Vec<Trytes81>, Vec<Hint>) {
         // if there is paging_state then we return hint to the user, to used for further API calls
         if let Some(paging_state) = self.metadata.take_paging_state() {
-            self.hint.paging_state.replace(paging_state);
+            self.hint.replace_paging_state(paging_state);
             self.hints.push(self.hint);
         }
         (self.hashes, self.hints)
@@ -106,7 +144,7 @@ impl HintsDecoder for Tx {
 
 pub fn query(hint: &mut Hint) -> Vec<u8> {
     let mut query_flags = SKIP_METADATA | VALUES | PAGE_SIZE;
-    if hint.paging_state.is_some() {
+    if hint.is_paging_state_some() {
         query_flags |= PAGING_STATE;
     }
     let Query(payload) = Query::new()
@@ -122,10 +160,10 @@ pub fn query(hint: &mut Hint) -> Vec<u8> {
         .query_flags(query_flags)
         .value_count(3)
         .value(hint.get_vertex()) // it might be tag or address
-        .value(hint.year)
-        .value(hint.month)
+        .value(hint.year())
+        .value(hint.month())
         .page_size(255 as i32)
-        .paging_state(&hint.paging_state.take())
+        .paging_state(&hint.take_paging_state())
         .build(MyCompression::get());
     payload
 }
