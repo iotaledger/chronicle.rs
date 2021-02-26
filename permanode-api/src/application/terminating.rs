c1827e1eea17cd77950cbd6b0c956e0a1cc3504b
Louay Kamel <lk@overdb.com>
2021-02-26 13:06:01 -0500

    Adjust PermanodeAPI name

--- permanode-api/src/application/terminating.rs
+++ permanode-api/src/application/terminating.rs
@@ -3,7 +3,6 @@ use super::*;
 #[async_trait]
 impl<H: PermanodeAPIScope> Terminating<H> for PermanodeAPI<H> {
     async fn terminating(&mut self, status: Result<(), Need>, supervisor: &mut Option<H>) -> Result<(), Need> {
-        println!("lol",);
         status
     }
 }
