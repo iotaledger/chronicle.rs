0e51a14d09515f2359da1f97ebd6fa4fb232c1ee
Louay Kamel <lk@overdb.com>
2021-04-14 16:20:27 -0400

    Update broker service tree

--- permanode-broker/src/solidifier/init.rs
+++ permanode-broker/src/solidifier/init.rs
@@ -7,6 +7,12 @@ use super::*;
 impl<H: PermanodeBrokerScope> Init<BrokerHandle<H>> for Solidifier {
     async fn init(&mut self, status: Result<(), Need>, _supervisor: &mut Option<BrokerHandle<H>>) -> Result<(), Need> {
         info!("{} is Initializing", self.get_name());
+        self.service.update_status(ServiceStatus::Initializing);
+        let event = BrokerEvent::Children(BrokerChild::Solidifier(self.service.clone(), Ok(())));
+        let _ = _supervisor
+            .as_mut()
+            .expect("Solidifier expected BrokerHandle")
+            .send(event);
         status
     }
 }
