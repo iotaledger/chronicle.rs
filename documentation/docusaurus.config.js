1c15c47c0c1867ae7ee46463364906294bc5a6ac
Dr-Electron <dr-electr0n@protonmail.com>
2022-04-11 07:56:23 +0000

    Prepare for new wiki build system

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    title: 'Chronicle',
+    url: '/',
+    baseUrl: '/',
+    themes: ['@docusaurus/theme-classic'],
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'chronicle-rs',
+                path: path.resolve(__dirname, './docs'),
+                routeBasePath: 'chronicle.rs',
+                sidebarPath: path.resolve(__dirname, './sidebars.js'),
+                editUrl: 'https://github.com/iotaledger/chronicle.rs/edit/main/',
+                remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, './static')],
+};
