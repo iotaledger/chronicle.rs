66508bbab0f8bd51eb004ad67ec6caf8b851b924
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-11 21:29:10 +0200

    Update docusaurus.config.js

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -22,7 +22,7 @@ module.exports = {
                 path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'chronicle.rs',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
-                editUrl: 'https://github.com/iotaledger/chronicle.rs/edit/main/',
+                editUrl: 'https://github.com/iotaledger/chronicle.rs/edit/main/documentation',
                 remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
             }
         ],
