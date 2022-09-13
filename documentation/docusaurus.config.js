b11ae7af1d103b232e38349ee748455d111b415a
Dr-Electron <dr-electr0n@protonmail.com>
2022-09-13 09:52:16 +0200

    Use chronicle path to be compatible with other versions

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -7,10 +7,16 @@ module.exports = {
             {
                 id: 'chronicle-rs',
                 path: path.resolve(__dirname, 'docs'),
-                routeBasePath: 'chronicle.rs',
+                routeBasePath: 'chronicle',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
                 editUrl: 'https://github.com/iotaledger/chronicle.rs/edit/main/documentation',
                 remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
+                versions: {
+                    current: {
+                        label: 'IOTA',
+                        badge: true
+                    },
+                },
             }
         ],
     ],
